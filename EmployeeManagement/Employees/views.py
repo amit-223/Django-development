eeeee88d4e265768ce45017e74be532f17d3fe40
amit-223 <143amitkarmakar@gmail.com>
2025-01-24 17:49:11 +0530

    changes

--- EmployeeManagement/Employees/views.py
+++ EmployeeManagement/Employees/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages 
 
 def home_view(request):
     return render(request, 'home.html/', {'message':'Welcome to Employee Management App!'})
 
 def login_view(request):
-    return render(request, 'login.html/', {'message': 'Welcome to login'})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home.html/')
+        else:
+            messages.error(request, 'Invalid user')
+    return render(request, 'login.html/')
+
 
