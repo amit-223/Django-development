dc4203e0d2660924adfe17c45fcd8c8313c344e6
Amit Karmakar <102965028+amit-223@users.noreply.github.com>
2025-01-29 23:33:02 +0530

    Delete flipkart directory

--- flipkart/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LoginAPIView,login_page
-
-urlpatterns = [
-    # path('login/', LoginAPIView.as_view(), name='login'),
-    path('login/', login_page, name='login_page'),  # Render login page
-    path('api/login/', LoginAPIView.as_view(), name='login'),  # API for login
-]
